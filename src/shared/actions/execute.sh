ce43dbe0ed723e6d4bf736ed318ecfa46b687692
David Robert Lewis <ubuntupunk@gmail.com>
2026-04-26 10:32:32 +0200

    Add global ~/.jq to jq library search path

--- src/shared/actions/execute.sh
+++ src/shared/actions/execute.sh
@@ -2,73 +2,62 @@
 # One should not have to use `jqnpm execute` when `jq` is enough.
 # https://github.com/joelpurra/jqnpm/blob/master/CONTRIBUTE.md#requirements-for-the-jq-binary
 function jqArgumentsContainFileArgument {
-	if arrayContainsValue "-f" "$@" || arrayContainsValue "--from-file" "$@" ]];
-	then
-		return 0;
-	fi
+  if arrayContainsValue "-f" "$@" || arrayContainsValue "--from-file" "$@" ]]; then
+    return 0
+  fi
 
-	return 1;
+  return 1
 }
 
 function execute {
-	if hasPackageMetadataFile && isDebugAtLevel 4;
-	then
-		# TODO: this array handling feels hacky.
-		# https://mywiki.wooledge.org/BashFAQ/020
-		unset directDependencyNames i
-		while IFS= read -r -d '' dependencyName; do
-			directDependencyNames[i++]="$dependencyName"
-		done < <(getDirectDependencyNames)
+  if hasPackageMetadataFile && isDebugAtLevel 4; then
+    # TODO: this array handling feels hacky.
+    # https://mywiki.wooledge.org/BashFAQ/020
+    unset directDependencyNames i
+    while IFS= read -r -d '' dependencyName; do
+      directDependencyNames[i++]="$dependencyName"
+    done < <(getDirectDependencyNames)
 
-		local numberOfDirectDependencyNames="${#directDependencyNames[@]}"
+    local numberOfDirectDependencyNames="${#directDependencyNames[@]}"
 
-		debugInPackageIfAvailable 4 "(preparing execute) numberOfDirectDependencyNames: ${numberOfDirectDependencyNames} directDependencyNames: '${directDependencyNames[@]}'"
-	fi
+    debugInPackageIfAvailable 4 "(preparing execute) numberOfDirectDependencyNames: ${numberOfDirectDependencyNames} directDependencyNames: '${directDependencyNames[@]}'"
+  fi
 
-	local usePackageMainJq="false"
+  local usePackageMainJq="false"
 
-	if jqArgumentsContainFileArgument "$@";
-	then
-		debugInPackageIfAvailable 4 "(preparing execute) call contains '-f' or '--from-file' argument, skipping checking for main jq file."
-	else
-		if hasValidPackageMainJq;
-		then
-			local mainPath=$(getValidPackageMainJq)
+  if jqArgumentsContainFileArgument "$@"; then
+    debugInPackageIfAvailable 4 "(preparing execute) call contains '-f' or '--from-file' argument, skipping checking for main jq file."
+  else
+    if hasValidPackageMainJq; then
+      local mainPath=$(getValidPackageMainJq)
 
-			debugInPackageIfAvailable 3 "(preparing execute) found the main jq file: '${mainPath}'"
+      debugInPackageIfAvailable 3 "(preparing execute) found the main jq file: '${mainPath}'"
 
-			usePackageMainJq="true"
-		else
-			debugInPackageIfAvailable 4 "(preparing execute) found no main jq file"
-		fi
-	fi
+      usePackageMainJq="true"
+    else
+      debugInPackageIfAvailable 4 "(preparing execute) found no main jq file"
+    fi
+  fi
 
-	if hasPackageMetadataFile && hasDirectDependencies;
-	then
-		if [[ "$usePackageMainJq" == "true" ]];
-		then
-			# TODO: don't *completely* redefine the orginal library path?
-			# The default contains useful, and therefore possibly widespread, defaults.
-			# The defaults are also system-dependent.
-
-			# Take care when editing the follow line, so debugging information and actual command stay in sync.
-			debugInPackageIfAvailable 5 "(executing jq)" jq -L "$localJqPackageBase" -f "$mainPath" "$@"
-			jq -L "$localJqPackageBase" -f "$mainPath" "$@"
-		else
-			# Take care when editing the follow line, so debugging information and actual command stay in sync.
-			debugInPackageIfAvailable 5 "(executing jq)" jq -L "$localJqPackageBase" "$@"
-			jq -L "$localJqPackageBase" "$@"
-		fi
-	else
-		if [[ "$usePackageMainJq" == "true" ]];
-		then
-			# Take care when editing the follow line, so debugging information and actual command stay in sync.
-			debugInPackageIfAvailable 5 "(executing jq)" jq -f "$mainPath" "$@"
-			jq -f "$mainPath" "$@"
-		else
-			# Take care when editing the follow line, so debugging information and actual command stay in sync.
-			debugInPackageIfAvailable 5 "(executing jq)" jq "$@"
-			jq "$@"
-		fi
-	fi
+  if hasPackageMetadataFile && hasDirectDependencies; then
+    if [[ "$usePackageMainJq" == "true" ]]; then
+      # Take care when editing the follow line, so debugging information and actual command stay in sync.
+      debugInPackageIfAvailable 5 "(executing jq)" jq -L "$localJqPackageBase" -L "$HOME/.jq" -f "$mainPath" "$@"
+      jq -L "$localJqPackageBase" -L "$HOME/.jq" -f "$mainPath" "$@"
+    else
+      # Take care when editing the follow line, so debugging information and actual command stay in sync.
+      debugInPackageIfAvailable 5 "(executing jq)" jq -L "$localJqPackageBase" "$@"
+      jq -L "$localJqPackageBase" "$@"
+    fi
+  else
+    if [[ "$usePackageMainJq" == "true" ]]; then
+      # Take care when editing the follow line, so debugging information and actual command stay in sync.
+      debugInPackageIfAvailable 5 "(executing jq)" jq -f "$mainPath" "$@"
+      jq -f "$mainPath" "$@"
+    else
+      # Take care when editing the follow line, so debugging information and actual command stay in sync.
+      debugInPackageIfAvailable 5 "(executing jq)" jq "$@"
+      jq "$@"
+    fi
+  fi
 }
